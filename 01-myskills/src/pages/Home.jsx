87b8819fd0c8d994a54cbed790fa3ae93374d014
Felipe Melo <narulee@hotmail.com>
2022-06-06 12:48:59 -0300

    create react-native-cli project

---
+++ 01-myskills/src/pages/Home.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import { View, StyleSheet, Text, TextInput, Platform, TouchableOpacity } from 'react-native'
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome, Felipe</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="New skill"
+        placeholderTextColor="#555"
+      />
+
+      <TouchableOpacity
+        activeOpacity={.7}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Add</Text>
+      </TouchableOpacity>
+
+      <Text style={[styles.title, { marginTop: 50 }]}>
+        My Skills
+      </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingHorizontal: 20,
+    paddingVertical: 70,
+    paddingHorizontal: 30
+  },
+
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+
+  input: {
+    backgroundColor: '#1f1e25',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7
+  },
+
+  button: {
+    backgroundColor: '#a370f7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+
+  buttonText: {
+    color: '#fff',
+    fontSize: 17,
+    fontWeight: 'bold',
+  }
+
+})
