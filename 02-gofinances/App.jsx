28da6d9d35569010d27ee1ed687a27f10a61e80f
Felipe Melo <narulee@hotmail.com>
2022-06-07 19:59:06 -0300

    02-gofinances: styled components and default theme

--- 02-gofinances/App.jsx
+++ 02-gofinances/App.jsx
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Poppins_400Regular, Poppins_500Medium, Poppins_700Bold, useFonts } from "@expo-google-fonts/poppins";
+import * as SplashScreen from 'expo-splash-screen';
+import { ThemeProvider } from "styled-components";
+
+import theme from "./src/global/styles/theme";
+import { Dashboard } from "./src/screens/Dashboard";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  SplashScreen.preventAutoHideAsync();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  })
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  SplashScreen.hideAsync();
+  
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  )
+}
