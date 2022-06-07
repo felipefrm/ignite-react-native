825809abb7622d38a1a043b18ff33c096383a555
Felipe Melo <narulee@hotmail.com>
2022-06-07 14:30:07 -0300

    challenge 01: done

---
+++ challenges/01-to.do/src/pages/Home.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import { Header } from '../components/Header';
+import { Task, TasksList } from '../components/TasksList';
+import { TodoInput } from '../components/TodoInput';
+
+export function Home() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  function handleAddTask(newTaskTitle: string) {
+    const newTask: Task = {
+      id: new Date().getTime(),
+      done: false,
+      title: newTaskTitle
+    }
+
+    setTasks([...tasks, newTask])
+  }
+
+  function handleToggleTaskDone(id: number) {
+    const updatedTasks = tasks.map(task => {
+      if (task.id === id) {
+        return {
+          ...task,
+          done: !task.done
+        }
+      }
+
+      return task
+    })
+
+    setTasks(updatedTasks)
+  }
+
+  function handleRemoveTask(id: number) {
+    setTasks(tasks.filter(task => task.id !== id))
+  }
+
+  return (
+    <View style={styles.container}>
+      <Header tasksCounter={tasks.length} />
+
+      <TodoInput addTask={handleAddTask} />
+
+      <TasksList
+        tasks={tasks}
+        toggleTaskDone={handleToggleTaskDone}
+        removeTask={handleRemoveTask}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EBEBEB'
+  }
+})
