08d85390233cdaa06937c4bf9acb02d253b420fa
Felipe Melo <narulee@hotmail.com>
2022-06-19 20:43:52 -0300

    02-gofinances: bottom tab navigation

--- 02-gofinances/App.jsx
+++ 02-gofinances/App.jsx
@@ -1,9 +1,10 @@
 import { Poppins_400Regular, Poppins_500Medium, Poppins_700Bold, useFonts } from "@expo-google-fonts/poppins";
+import { NavigationContainer } from "@react-navigation/native";
 import * as SplashScreen from 'expo-splash-screen';
 import { ThemeProvider } from "styled-components";
 
 import theme from "./src/global/styles/theme";
-import { Dashboard } from "./src/screens/Dashboard";
+import { AppRoutes } from "./src/routes/app.routes";
 
 export default function App() {
   SplashScreen.preventAutoHideAsync();
@@ -19,10 +20,12 @@ export default function App() {
   }
 
   SplashScreen.hideAsync();
-  
+
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   )
 }
