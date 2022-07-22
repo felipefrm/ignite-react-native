40c73a7b078d30c33eb31c760098eadaba30ec83
Felipe Melo <felipefrmelo@hotmail.com>
2022-07-21 22:20:15 -0300

    03-rentx: create Header styled component

--- 03-rentx/src/screens/Home/index.tsx
+++ 03-rentx/src/screens/Home/index.tsx
@@ -1,9 +1,26 @@
-import { Container, Title } from "./styles";
+import { StatusBar } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+
+import Logo from '../../assets/logo.svg';
+
+import { Container, Header, HeaderContent, TotalCars } from "./styles";
 
 export function Home() {
   return (
     <Container>
-      <Title>HOME</Title>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <Logo width={RFValue(108)} heigth={RFValue(12)} />
+          <TotalCars>
+            Total de 12 carros
+          </TotalCars>
+        </HeaderContent>
+      </Header>
     </Container>
   )
 }
