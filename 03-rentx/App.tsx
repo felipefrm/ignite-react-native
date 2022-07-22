917ffca0138159ff8e4f01637ea1d6a6540f249c
Felipe Melo <felipefrmelo@hotmail.com>
2022-07-21 21:45:25 -0300

    03-rentx: defined global styles

--- 03-rentx/App.tsx
+++ 03-rentx/App.tsx
@@ -1,7 +1,40 @@
+import * as SplashScreen from 'expo-splash-screen';
+import { ThemeProvider } from 'styled-components';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from '@expo-google-fonts/inter'
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold
+} from '@expo-google-fonts/archivo'
+
 import { Home } from './src/screens/Home';
 
+import theme from './src/styles/theme';
+
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
+
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold
+  })
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  SplashScreen.hideAsync();
+
   return (
-   <Home />
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   );
 }
