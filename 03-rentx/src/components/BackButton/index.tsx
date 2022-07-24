27cc35829bbab6cf3a844ab459bfe877de60079a
Felipe Melo <felipefrmelo@hotmail.com>
2022-07-23 21:41:25 -0300

    03-rentx: create car detail interface

---
+++ 03-rentx/src/components/BackButton/index.tsx
@@ -0,0 +1,23 @@
+import { TouchableOpacityProps } from "react-native";
+import { useTheme } from "styled-components";
+import { MaterialIcons } from "@expo/vector-icons";
+
+import { Container } from "./styles";
+
+interface BackButtonProps extends TouchableOpacityProps {
+  color?: string;
+}
+
+export function BackButton({ color, ...rest }: BackButtonProps) {
+  const theme = useTheme();
+
+  return (
+    <Container {...rest}>
+      <MaterialIcons
+        name="chevron-left"
+        size={24}
+        color={color ? color : theme.colors.text}
+      />
+    </Container>
+  )
+}
