43d667f629ede9f5595c3e3b306e164a36e3efc2
Felipe Melo <felipefrmelo@hotmail.com>
2022-09-26 21:59:23 -0300

    03-rentx: create scheduling interface

---
+++ 03-rentx/src/screens/Scheduling/index.tsx
@@ -0,0 +1,65 @@
+import { useTheme } from "styled-components";
+import { StatusBar } from "react-native";
+
+import { BackButton } from "../../components/BackButton";
+import { Button } from "../../components/Button";
+import { Calendar } from "../../components/Calendar";
+
+import {
+  Container,
+  Header,
+  Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  Content,
+  Footer,
+} from "./styles";
+
+import ArrowSvg from "../../assets/arrow.svg";
+
+export function Scheduling() {
+  const theme = useTheme()
+
+  return (
+    <Container>
+      <Header>
+        <StatusBar
+          barStyle="light-content"
+          translucent
+          backgroundColor="transparent"
+        />
+        <BackButton onPress={() => { }} color={theme.colors.shape} />
+
+        <Title>
+          Escolha uma {'\n'}
+          data de início e {'\n'}
+          fim do aluguel
+        </Title>
+
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue selected={false}>18/05/2012</DateValue>
+          </DateInfo>
+
+          <ArrowSvg />
+
+          <DateInfo>
+            <DateTitle>ATÉ</DateTitle>
+            <DateValue selected={false}>18/05/2012</DateValue>
+          </DateInfo>
+        </RentalPeriod>
+      </Header>
+
+      <Content>
+        <Calendar />
+      </Content>
+
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
+    </Container>
+  );
+}
