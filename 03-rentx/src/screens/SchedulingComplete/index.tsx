5ea9a649966e0ee45650151de5cdc3cba45636f7
Felipe Melo <felipefrmelo@hotmail.com>
2022-11-09 21:41:17 -0300

    create SchedulingComplete interface

---
+++ 03-rentx/src/screens/SchedulingComplete/index.tsx
@@ -0,0 +1,39 @@
+import { StatusBar, useWindowDimensions } from 'react-native';
+
+import { ConfirmButton } from '../../components/ConfirmButton';
+
+import { Container, Content, Footer, Message, Title } from './styles';
+
+import LogoSvg from '../../assets/logo_background_gray.svg';
+import DoneSvg from '../../assets/done.svg';
+
+export function SchedulingComplete() {
+  const { width } = useWindowDimensions();
+
+  return (
+    <Container>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
+
+      <LogoSvg width={width} />
+
+      <Content>
+        <DoneSvg width={80} height={80} />
+
+        <Title>Carro alugado!</Title>
+        <Message>
+          Agora você só precisa ir {'\n'}
+          até a concessionária da RENTX {'\n'}
+          pegar o seu automóvel.
+        </Message>
+      </Content>
+
+      <Footer>
+        <ConfirmButton title="OK" />
+      </Footer>
+    </Container>
+  )
+}
