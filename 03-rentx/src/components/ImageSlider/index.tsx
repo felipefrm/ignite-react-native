570e95fd373009f33d4335805e0d129fbdf44ce5
Felipe Melo <felipefrmelo@hotmail.com>
2022-11-20 20:40:26 -0300

    03-rentx: image slider

--- 03-rentx/src/components/ImageSlider/index.tsx
+++ 03-rentx/src/components/ImageSlider/index.tsx
@@ -1,3 +1,5 @@
+import { useRef, useState } from "react";
+import { FlatList, ViewToken } from "react-native";
 import {
   Container,
   ImageIndexes,
@@ -10,23 +12,44 @@ interface ImageSliderProps {
   imagesUrl: string[];
 }
 
+interface ChangeImageProps {
+  viewableItems: ViewToken[];
+  changed: ViewToken[];
+}
+
 export function ImageSlider({ imagesUrl }: ImageSliderProps) {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const indexChanged = useRef((info: ChangeImageProps) => {
+    const index = info.viewableItems[0].index!;
+    setActiveIndex(index);
+  });
 
   return (
     <Container>
       <ImageIndexes>
-        <ImageIndex active={true} />
-        <ImageIndex active={false} />
-        <ImageIndex active={false} />
-        <ImageIndex active={false} />
+        {
+          imagesUrl.map((_, index) => (
+            <ImageIndex key={index} active={index === activeIndex} />
+          ))
+        }
       </ImageIndexes>
 
-      <CarImageWrapper>
-        <CarImage
-          source={{ uri: imagesUrl[0] }}
-          resizeMode="contain"
-        />
-      </CarImageWrapper>
+      <FlatList
+        data={imagesUrl}
+        keyExtractor={key => key}
+        renderItem={({ item }) => (
+          <CarImageWrapper>
+            <CarImage
+              source={{ uri: item }}
+              resizeMode="contain"
+            />
+          </CarImageWrapper>
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        onViewableItemsChanged={indexChanged.current}
+      />
     </Container>
   )
 }
